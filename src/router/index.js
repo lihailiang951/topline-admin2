c9f73f146bca88ba981b018e44dc4e79df9b3836
尹大炮 <3235144364@qq.com>
2019-08-19 16:54:38 +0800

    Layout布局组件-创建并配置layout路由组件

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,18 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   name: 'home',
+    //   path: '/',
+    //   // 无论你当前模块在哪里，@都可以直接定位到 src
+    //   // 加载一个目录可以默认加载他的 index.js index.vue index.json...
+    //   // component: () => import('@/views/home/index.vue')
+    //   component: () => import('@/views/home')
+    // },
     {
-      name: 'home',
+      name: 'layout',
       path: '/',
-      // 无论你当前模块在哪里，@都可以直接定位到 src
-      // 加载一个目录可以默认加载他的 index.js index.vue index.json...
-      // component: () => import('@/views/home/index.vue')
-      component: () => import('@/views/home')
+      component: () => import('@/views/layout')
     },
     {
       name: 'login',
---
+++ src/views/layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout-wrap">
+    <AppAside/>
+    <AppHeader/>
+    <!-- HOME、内容管理、粉丝管理。。。。都显示到这里 -->
+  </div>
+</template>
+
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHeader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" secoped>
+
+</style>
